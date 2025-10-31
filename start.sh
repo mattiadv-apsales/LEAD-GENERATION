851c50c464d6cb7d9ccd48ff6449778b47780042
mattiadv-apsales <mattia.dv@andreicopy.com>
2025-10-31 17:58:26 +0100

    update V1

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# installa Chromium per Playwright
-python -m playwright install chromium
-# avvia Flask tramite Gunicorn
-gunicorn server:app --bind 0.0.0.0:$PORT --workers 1 --threads 4
